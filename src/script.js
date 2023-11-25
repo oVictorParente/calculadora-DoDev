6753f2cb19af2d8c189c6ac59561a678cec93442
Victor Parente <ovictorparente@outlook.com>
2023-11-25 17:21:01 -0300

    Criação de função para funcionamento da Calculadora

--- src/script.js
+++ src/script.js
@@ -1,7 +1,38 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(numero1, numero2, operador) {
+  let num1 = parseInt(numero1)
+  let num2 = parseInt(numero2)
+  let resultado
+  
+  switch (operador){
+    case "+": 
+      resultado = num1 + num2
+      break;
+    case "-": 
+      resultado = num1 - num2
+      break;
+    case "/": 
+      resultado = num1 / num2
+      break;
+    case "*": 
+      resultado = num1 * num2
+      break;
+    case "e":
+      let contador = 1
+      resultado = num1 * num1
+      while(contador < num2 - 1) {
+        resultado *= num1
+        contador++
+      }
+      break;
+  }
+  if (resultado >= 1000000 || resultado == undefined) {
+    resultado = "ERRO"
+  }
+    return resultado
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
